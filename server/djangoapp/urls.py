60e1b18b564755ba965adedcdc607ceb9ee947ff
youssefwerrad <youssefwerrad@gmail.com>
2025-12-22 18:26:51 +0000

    Describe the changes you made

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -12,7 +12,8 @@ urlpatterns = [
     path(route='login', view=views.login_user, name='login'),
     # path for logout
     path(route='logout', view=views.logout_request, name='logout'),
-
+    # path for get cars view
+    path(route='get_cars', view=views.get_cars, name ='getcars'),
     # path for dealer reviews view
 
     # path for add a review view
