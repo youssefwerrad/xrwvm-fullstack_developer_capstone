136a30c41418549042f733d7fa9fe91f015d5290
youssefwerrad <youssefwerrad@gmail.com>
2025-12-23 15:22:27 +0000

    Tue, Dec 23, 2025, 3:22 PM +00:00

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -78,8 +78,18 @@ def registration(request):
 
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
-# def get_dealer_reviews(request,dealer_id):
-# ...
+def get_dealer_reviews(request, dealer_id):
+    # if dealer id has been provided
+    if(dealer_id):
+        endpoint = "/fetchReviews/dealer/"+str(dealer_id)
+        reviews = get_request(endpoint)
+        for review_detail in reviews:
+            response = analyze_review_sentiments(review_detail['review'])
+            print(response)
+            review_detail['sentiment'] = response['sentiment']
+        return JsonResponse({"status":200,"reviews":reviews})
+    else:
+        return JsonResponse({"status":400,"message":"Bad Request"})
 
 # Create a `get_dealer_details` view to render the dealer details
 # def get_dealer_details(request, dealer_id):
