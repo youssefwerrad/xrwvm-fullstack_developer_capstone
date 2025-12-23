41edd0706fa50da09ca04f293dcc993c9b5198d4
youssefwerrad <youssefwerrad@gmail.com>
2025-12-23 06:59:50 -0500

    Your commit message

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -14,7 +14,11 @@ urlpatterns = [
     path(route='logout', view=views.logout_request, name='logout'),
     # path for get cars view
     path(route='get_cars', view=views.get_cars, name ='getcars'),
+    # 
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     # path for dealer reviews view
+
 
     # path for add a review view
 
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -75,7 +75,7 @@ def registration(request):
 # # Update the `get_dealerships` view to render the index page with
 # a list of dealerships
 # def get_dealerships(request):
-# ...
+
 
 # Create a `get_dealer_reviews` view to render the reviews of a dealer
 # def get_dealer_reviews(request,dealer_id):
