2d59ffb89894db7f8083b702b6cd102f359eafc7
youssef werrad <youssefwerrad@gmail.com>
2026-01-09 12:23:31 -0500

    Create Django Proxy Services Of Backend APIs

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -8,15 +8,20 @@ app_name = 'djangoapp'
 urlpatterns = [
     # path for registration
     path(route='register', view=views.registration, name='register'),
-
     # path for login
     path(route='login', view=views.login_user, name='login'),
     # path for logout
     path(route='logout', view=views.logout_request, name='logout'),
     # path for dealer reviews view
-
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
     # path for add a review view
-    
+    path(route='add_review', view=views.add_review, name='add_review'),    
     # path for get_cars
     path(route='get_cars', view=views.get_cars, name ='getcars'),
+    # path for get_dealers
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    # path for get_dealers state
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
+    # path for get_dealer details
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
